2edb0f05d49722e9cdc597439f66e4bf5d539b65
mayankvyas1306 <vyas79303@gmail.com>
2025-03-10 17:42:47 +0530

    Second phase done

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -7,8 +7,11 @@ using namespace std;
 
 const int SIZE = 9; // Board size
 const int MINES = 10; // Number of mines
-vector<vector<char>> board(SIZE, vector<char>(SIZE, '-'));
+vector<vector<char>> board(SIZE, vector<char>(SIZE, '-') );
 vector<vector<int>> mineBoard(SIZE, vector<int>(SIZE, 0)); // Stores mine info
+vector<vector<bool>> flagged(SIZE, vector<bool>(SIZE, false)); // Flagged cells
+bool gameOver = false; // Game over flag
+int flaggedMines = 0;
 
 // Function to print the board
 void printBoard() {
@@ -68,7 +71,8 @@ void revealCells(int x, int y) {
     if (x < 0 || x >= SIZE || y < 0 || y >= SIZE || board[x][y] != '-') return;
     if (mineBoard[x][y] == -1) {
         cout << "Game Over! You hit a mine!\n";
-        exit(0);
+        gameOver = true;
+        return;
     }
     board[x][y] = mineBoard[x][y] + '0';
     if (mineBoard[x][y] == 0) {
@@ -85,22 +89,58 @@ void revealCells(int x, int y) {
 bool checkWin() {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            if (board[i][j] == '-' && mineBoard[i][j] != -1) return false;
+            if ((board[i][j] == '-' && mineBoard[i][j] != -1) || (flagged[i][j] && mineBoard[i][j] != -1))
+                return false;
         }
     }
     return true;
 }
 
+// Function to toggle flag on a cell
+void toggleFlag(int x, int y) {
+    if (board[x][y] == '-') {
+        board[x][y] = 'F';
+        flagged[x][y] = true;
+        if (mineBoard[x][y] == -1) flaggedMines++;
+    } else if (board[x][y] == 'F') {
+        board[x][y] = '-';
+        flagged[x][y] = false;
+        if (mineBoard[x][y] == -1) flaggedMines--;
+    }
+}
+
 // Main game loop
 void playGame() {
     initializeBoard();
-    while (true) {
+    while (!gameOver) {
         printBoard();
         int x, y;
-        cout << "Enter row and column (x y): ";
-        cin >> x >> y;
-        revealCells(x, y);
+        char action;
+        cout << "Enter row, column, and action (r c [r/f]): ";
+        cin >> x >> y >> action;
+        
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
+            cout << "Invalid input! Please enter values between 0 and " << SIZE-1 << "\n";
+            continue;
+        }
+        
+        if (action == 'r') {
+            revealCells(x, y);
+        } else if (action == 'f') {
+            toggleFlag(x, y);
+        } else {
+            cout << "Invalid action! Use 'r' to reveal or 'f' to flag.\n";
+            continue;
+        }
+        
+        if (gameOver) {
+            printBoard();
+            cout << "Game Over! Better luck next time.\n";
+            break;
+        }
+        
         if (checkWin()) {
+            printBoard();
             cout << "Congratulations! You won the game!\n";
             break;
         }
